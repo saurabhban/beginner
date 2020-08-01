cb7005c631efd6587cb4dc660db2960ba5af10c7
saurabh banerjee <saurabh.b.banerjee@oracle.com>
2020-08-01 12:54:46 +0530

    check

---
+++ codes/ex29.py
@@ -0,0 +1,19 @@
+print (f"lets play game on if and else statement")
+dogs= 20
+man =30
+cat =10
+
+if (dogs > man):
+	print("world is drooled")
+
+if (man > dogs):
+	print("humanity is saved")
+
+if (cat > dogs):
+	print("too much meow")
+
+
+dogs+=30
+
+if (dogs > man):
+	print("all humans are turning out to be dogs")
